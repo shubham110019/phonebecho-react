9dbc6aae50dc457dbf3184e1d901ab42e64f9d8a
shubham ralli <sralli73@gmail.com>
2022-06-12 22:47:57 +0530

    admin panel model phone addd

--- src/admin/brand/Addbrand.js
+++ src/admin/brand/Addbrand.js
@@ -107,112 +107,111 @@ const Addbrand = () => {
         <Topmenu />
 
         <div className="home-content">
-      <div className="container px-4">
-        <div className="row">
-          <div className="col-md-5 p-2">
-            <div className="border p-3 bg-white">
-            <p>{checkfrom}</p>
+          <div className="container px-4">
+            <div className="row">
+              <div className="col-md-5 p-2">
+                <div className="border p-3 bg-white">
+                  <p>{checkfrom}</p>
 
-            {updatecheck ? (
-              <h2 className="mb-4 r">Update brand</h2>
-            ) : (
-              <h2 className="mb-4 r">Add brand </h2>
-            )}
+                  {updatecheck ? (
+                    <h2 className="mb-4 r">Update brand</h2>
+                  ) : (
+                    <h2 className="mb-4 r">Add brand </h2>
+                  )}
 
-            <div className="mb-3">
-              <label className="form-label">Brand</label>
-              <input
-                type="text"
-                className="form-control"
-                value={brand}
-                onChange={(e) => setBrand(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Image link</label>
-              <input
-                type="text"
-                className="form-control"
-                value={image}
-                onChange={(e) => setImage(e.target.value)}
-              />
-            </div>
+                  <div className="mb-3">
+                    <label className="form-label">Brand</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={brand}
+                      onChange={(e) => setBrand(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Image link</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={image}
+                      onChange={(e) => setImage(e.target.value)}
+                    />
+                  </div>
 
-            {updatecheck ? (
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={() => {
-                  brandupd(brandidc);
-                }}
-              >
-                Update
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={() => {
-                  brandsubmit();
-                }}
-              >
-                Submit
-              </button>
-            )}
-          </div>
+                  {updatecheck ? (
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      onClick={() => {
+                        brandupd(brandidc);
+                      }}
+                    >
+                      Update
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      onClick={() => {
+                        brandsubmit();
+                      }}
+                    >
+                      Submit
+                    </button>
+                  )}
+                </div>
               </div>
 
-
-          <div className="col-md-7 p-2">
-          <div className="border p-3 bg-white">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Brand</th>
-                  <th>status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {brandapi
-                  ? brandapi.map((item, i) => {
-                      return (
-                        <>
-                          <tr>
-                            <td>
-                              {i} {item.id}
-                            </td>
-                            <td>{item.brand}</td>
-                            <td>
-                              <button
-                                className="btn btn-info btn-sm mx-2"
-                                onClick={() => {
-                                  updateBrand(item._id);
-                                }}
-                              >
-                                edit
-                              </button>
-                              <button
-                                className="btn btn-danger btn-sm"
-                                onClick={() => {
-                                  deleteBrand(item._id);
-                                }}
-                              >
-                                delete
-                              </button>
-                            </td>
-                          </tr>
-                        </>
-                      );
-                    })
-                  : null}
-              </tbody>
-            </table>
-          </div>
+              <div className="col-md-7 p-2">
+                <div className="border p-3 bg-white">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Brand</th>
+                        <th>status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {brandapi
+                        ? brandapi.map((item, i) => {
+                            return (
+                              <>
+                                <tr>
+                                  <td>
+                                    {i} {item.id}
+                                  </td>
+                                  <td>{item.brand}</td>
+                                  <td>
+                                    <button
+                                      className="btn btn-info btn-sm mx-2"
+                                      onClick={() => {
+                                        updateBrand(item._id);
+                                      }}
+                                    >
+                                      edit
+                                    </button>
+                                    <button
+                                      className="btn btn-danger btn-sm"
+                                      onClick={() => {
+                                        deleteBrand(item._id);
+                                      }}
+                                    >
+                                      delete
+                                    </button>
+                                  </td>
+                                </tr>
+                              </>
+                            );
+                          })
+                        : null}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
       </section>
     </>
   );
--- src/admin/model/Add-model.js
+++ src/admin/model/Add-model.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import Menu from "../cmp/Menu";
 import Topmenu from "../cmp/Topmenu";
 
 const Addmodel = () => {
+
+  const [apibrand, setApibrand] = useState();
+
+  const apiData = async () => {
+    const apidata = await fetch('http://localhost:9000/phone/');
+    // setData(await apidata.json());
+    const brandapidata = await apidata.json();
+    const fullbrand = brandapidata.brandapi;
+    setApibrand(fullbrand);
+}
+
+useEffect(()=>{
+  apiData();
+},[])
+
   return (
     <>
       <Menu />
@@ -11,8 +26,317 @@ const Addmodel = () => {
         <Topmenu />
 
         <div className="home-content">
-          <div className="container">
+          <div className="container px-4">
             <h2>Add Model</h2>
+
+              <div className="row">
+              <div className="col-md-12 p-2">
+                <div className="border p-3 bg-white row">
+
+                <div className="mb-3 col-md-4">
+                    <label className="form-label">Model Name</label>
+                    <select class="form-select" aria-label="Default select example">
+                    {
+                                      apibrand ?
+                                      apibrand.map((item,i)=>{
+                                      return(<>
+                                      <option value={item.brand} key={i}>{item.brand}</option>
+                                      </>
+                                      )}):<>data not fetch</>
+                                }
+                    </select>
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Model Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">url</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Price</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-6">
+                    <label className="form-label">Image link</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col-md-12">
+                    <h4 className="my-4">Functional or Physical Problems</h4>
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Display not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Display Glass</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Front Camera not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Back Camera not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Volume Button not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Finger Touch not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">WiFi not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Battery Faulty</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Speaker Faulty</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Power Button not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Charging Port not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Face Sensor not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Silent Button not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Audio Receiver not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Camera Glass Broken</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Bluetooth not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Vibrator is not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Microphone not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Proximity Sensor not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Audio Jack not working</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col-md-12">
+                    <h4 className="my-4">Accessories</h4>
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Box</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Original Charger</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col-md-12">
+                    <h4 className="my-4">Bill</h4>
+                  </div>
+
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">Below 3 months</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">3 months - 6 months</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">6 months - 11 months</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">Above 11 months</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col-md-12">
+                    <h4 className="my-4">Condition</h4>
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Good</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">below</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="mb-3 col-md-4">
+                    <label className="form-label">Not Good</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                    />
+                  </div>
+
+
+                  <div className="mb-3 col-md-12">
+                   <button className="btn btn-info">submit</button>
+                  </div>
+                  
+
+             
+                </div>
+              </div>
+              </div>
+
           </div>
         </div>
       </section>
