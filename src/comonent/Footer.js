6ff63dfdefc70ed335f76791516456ec0c41ef8c
shubham.ralli <shubham.ralli@socialpilot.co>
2022-09-19 20:30:04 +0530

    footer change

--- src/comonent/Footer.js
+++ src/comonent/Footer.js
@@ -4,55 +4,11 @@ const Footer = () => {
     return (
         <>
            
-                <footer className="py-5 bg-grey">
+                <footer className="py-4 bg-grey">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-3">
-                            <h5>Section</h5>
-                            <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-                            </ul>
+                            <div className='col-md-12 text-center'><p className='mb-0'>Copyright @ 2022 PhoneBecho All rights reserved</p></div>
                         </div>
-
-                        <div className="col-lg-3">
-                            <h5>Section</h5>
-                            <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3">
-                            <h5>Section</h5>
-                            <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3">
-                            <h5>Section</h5>
-                            <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-                            </ul>
-                        </div>
-                        
-                    </div>
-
                     </div>
                 </footer>
            
